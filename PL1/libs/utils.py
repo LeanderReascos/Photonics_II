931df39ef35e9e7a2c442817b565079870cd5666
Leander Reascos <reascoslea@gmail.com>
2022-04-06 21:41:31 +0100

    Add Irradiance calculation

--- PL1/libs/utils.py
+++ PL1/libs/utils.py
@@ -3,12 +3,12 @@ import matplotlib.pyplot as plt
 import re
 
 class EXPERIMENT:
-    def __init__(self,material,group,type,power):
+    def __init__(self,material,group,aperture,power):
         PATH = 'Data/'
-        file = f'zscan_{material}_300322_g{group}_{type}_{power}mW/'
+        file = f'zscan_{material}_300322_g{group}_{aperture}_{power}mW/'
         self.power = power*1e-3
         self.material = material
-        self.type = type
+        self.aperture = aperture
         self.positions = np.load(open(PATH+file+file[:-1]+'_dl_pos.npy','rb'))
         self.power_points = np.load(open(PATH+file+file[:-1]+'_pwr.npy','rb'))
         self.log_file = open(PATH+file+file[:-1]+'_log.csv','r').read()
@@ -18,8 +18,11 @@ class EXPERIMENT:
         self.I = self.power_points/A*self.log['exposure_time']
         self.I0 = self.power/A*self.log['exposure_time']
 
+        print(f'Material: {material} Aperture: {aperture} Power: {power}')
+        print(f'\tI0 = {self.I0}')
+
     def plot_power(self,title=None):
-        title = f'{self.material}-{self.type},{self.power}mW' if title is None else title
+        title = f'{self.material}-{self.aperture},{self.power}mW' if title is None else title
         fig,ax = plt.subplots()
         ax.set_title(title,fontsize=16)
         ax.plot(self.positions,self.power_points)
