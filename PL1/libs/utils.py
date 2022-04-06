45e1ec3e59cfabc432ebf3662e4ff0f5cffc01ec
Leander Reascos <reascoslea@gmail.com>
2022-04-06 21:28:19 +0100

    Add Irradiance calculation

--- PL1/libs/utils.py
+++ PL1/libs/utils.py
@@ -6,7 +6,7 @@ class EXPERIMENT:
     def __init__(self,material,group,type,power):
         PATH = 'Data/'
         file = f'zscan_{material}_300322_g{group}_{type}_{power}mW/'
-        self.power = power
+        self.power = power*1e-3
         self.material = material
         self.type = type
         self.positions = np.load(open(PATH+file+file[:-1]+'_dl_pos.npy','rb'))
@@ -14,6 +14,9 @@ class EXPERIMENT:
         self.log_file = open(PATH+file+file[:-1]+'_log.csv','r').read()
         values = ['start_pos','end_pos','step','exposure_time','avg_Power','aperture','no_lens']
         self.log = dict(zip(values,[float(n) for n in re.findall(r'\d+| \d+\.\d+|\.\d+',self.log_file)]))
+        A = (self.log['aperture']*1e-3)**2*np.pi
+        self.I = self.power_points/A*self.log['exposure_time']
+        self.I0 = self.power/A*self.log['exposure_time']
 
     def plot_power(self,title=None):
         title = f'{self.material}-{self.type},{self.power}mW' if title is None else title
