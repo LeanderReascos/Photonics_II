Correct n2 calculation and compute n2 for all cg data

PL1/libs/utils.py
@@ -3,29 +3,25 @@ import matplotlib.pyplot as plt
 import re
 
 class DATA:
-    def __init__(self,material,group,aperture,power,ra):
+    def __init__(self,material,group,aperture,power):
         PATH = 'Data/'
         file = f'zscan_{material}_300322_g{group}_{aperture}_{power}mW/'
         self.power = power*1e-3
         self.material = material
         self.aperture = aperture
-        self.ra = ra
+        
         self.positions = np.load(open(PATH+file+file[:-1]+'_dl_pos.npy','rb'))
-        self.positions = np.abs(self.positions-np.max(self.positions))*1e-3
+        self.positions = np.abs(self.positions-np.max(self.positions))*1e-3 #meters
 
-        self.power_points = np.load(open(PATH+file+file[:-1]+'_pwr.npy','rb'))
+        self.power_points = np.load(open(PATH+file+file[:-1]+'_pwr.npy','rb')) #Watts
         self.log_file = open(PATH+file+file[:-1]+'_log.csv','r').read()
         values = ['start_pos','end_pos','step','exposure_time','avg_Power','aperture','no_lens']
         self.log = dict(zip(values,[float(n) for n in re.findall(r'\d+| \d+\.\d+|\.\d+',self.log_file)]))
-        A = (ra)**2*np.pi
-        self.I = self.power_points/A
-        self.I0 = self.power/A
 
-        self.T = self.I/self.I0 #Trasmitance
+        self.T = self.power_points/self.power #Trasmitance
         self.DT = self.T/self.T[-1] #T/T(Z>>Z0)
 
         print(f'Material: {material} Aperture: {aperture} Power: {power}')
-        print(f'\tI0 = {self.I0}')
     
     def plot_power(self,title=None):
         title = f'Power Plot\n{self.material}-{self.aperture},{self.power}W' if title is None else title
@@ -41,8 +37,8 @@ class DATA:
         ax.plot(self.positions,self.DT)
 
 class CLOSE(DATA):
-    def __init__(self, material, group, aperture, power, ra):
-        super().__init__(material, group, aperture, power, ra)
+    def __init__(self, material, group, aperture, power):
+        super().__init__(material, group, aperture, power)
 
         self.Tp = np.max(self.DT)
         self.Tv = np.min(self.DT)
@@ -55,33 +51,35 @@ class CLOSE(DATA):
         p_max = np.max([self.iTv,self.iTp])
         self.Zfocal_plane = self.positions[p_min]+(self.positions[p_max]-self.positions[p_min])/2
         self.iZfocal_plane = np.argmin(np.abs(self.DT[p_min:p_max]-1))+p_min
-        self.I0_focalPlane = self.I[self.iZfocal_plane]
         self.positions -= self.Zfocal_plane
 
-        self.z0 = np.abs(self.positions[p_max]-self.positions[p_min])/1.7
+        self.z0 = np.abs(self.positions[p_max]-self.positions[p_min])/1.7 #meters
 
-        print(f'Position of the focal plane: {self.Zfocal_plane}, {self.positions[self.iZfocal_plane]}')
-        print(f'I0 focal plane: {self.I0_focalPlane}')
-        print(f'Rayleigh range: {self.z0}')
+        print(f'Position of the focal plane: {self.Zfocal_plane} m, {self.positions[self.iZfocal_plane]} m')
+        print(f'Rayleigh range: {self.z0} m')
 
     def set_S(self,Topen):
         self.S = self.T[-1]/Topen
         print(f'S: {self.S}')
     
     def set_expValues(self,wave_lenght,L_material):
-        self.wave_lenght = wave_lenght
-        self.L = L_material
+        self.wave_lenght = wave_lenght #meters
+        self.L = L_material #meters
+
+        self.w0 = np.sqrt(self.wave_lenght*self.z0/np.pi) #meters^2
+        self.I0 = 2*self.power/(np.pi*self.w0**2) #W/m^2
+        print(f'W0: {self.w0} m^2\nI0: {self.I0} W/m^2')
 
     def calc_n2(self):
         self.DPhi = self.DTpv/(0.406*(1-self.S)**0.27)
-        self.n2 = (self.wave_lenght/(2*np.pi))*self.DPhi/(self.I0_focalPlane*self.L)
+        self.n2 = (self.wave_lenght/(2*np.pi))*self.DPhi/(self.I0*self.L)
         print(f'DPhi: {self.DPhi}')
-        print(f'n2: {self.n2}')
+        print(f'n2: {self.n2} m^2/W')
 
 
 class OPEN(DATA):
-    def __init__(self, material, group, aperture, power, ra):
-        super().__init__(material, group, aperture, power, ra)
+    def __init__(self, material, group, aperture, power):
+        super().__init__(material, group, aperture, power)
 
     def set_focalPlane(self,Zfolcal_plane):
         self.Zfocal_plane = Zfolcal_plane
@@ -89,12 +87,12 @@ class OPEN(DATA):
     
 
 class EXPERIMENT:
-    def __init__(self,material,group,power,r=[2e-3,12e-3]):
+    def __init__(self,material,group,power):
         self.material = material
         self.group = group
         self.power = power
-        self.close = CLOSE(material,group,'close',power,r[0])
-        self.open = OPEN(material,group,'open',power,r[1])
+        self.close = CLOSE(material,group,'close',power)
+        self.open = OPEN(material,group,'open',power)
         self.open.set_focalPlane(self.close.Zfocal_plane)
         self.close.set_S(self.open.T[-1])
     